c18a1598d21a95dd5f57c41c659ef92012fb24f9
Nora Balaisyte <nbalaisyte@spartaglobal.com>
2019-10-15 15:32:12 +0100

    New Pages folder

---
+++ Engineering40AutomationPracticeProject/Steps/AddToCartSteps.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Engineering40AutomationPracticeProject
+{
+    [Binding]
+    public class AddToCartSteps
+    {
+        [Given(@"I am on the home page")]
+        public void GivenIAmOnTheHomePage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have opted an item")]
+        public void GivenIHaveOptedAnItem()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have chosen the colour")]
+        public void GivenIHaveChosenTheColour()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have picked the size")]
+        public void GivenIHavePickedTheSize()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have added the quantity")]
+        public void GivenIHaveAddedTheQuantity()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I press the add to the cart button")]
+        public void WhenIPressTheAddToTheCartButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should be able to to see the item added to the cart")]
+        public void ThenIShouldBeAbleToToSeeTheItemAddedToTheCart()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
