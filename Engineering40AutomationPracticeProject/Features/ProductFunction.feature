3c2903a0a516884e233ab47c900be7aaa2ab0311
Eduard Pavliuc <epavliuc@spartaglobal.com>
2019-10-15 16:49:03 +0100

    added productfunction feature and productfunction steps

---
+++ Engineering40AutomationPracticeProject/Features/ProductFunction.feature
@@ -0,0 +1,24 @@
+﻿Feature: ProductFunction
+As a user
+I want to be able to add products to the wishlist
+I want to be able to compare products
+
+Scenario: Valid Add to the Wishlist 
+	Given That I am on the product page
+	And I am logged in
+	And I have selected a product
+	When I press on the wishlist button
+	Then I should see the product on the wishlist
+
+Scenario: Invalid Add to the Wishlist 
+	Given That I am on the product page
+	And I have selected a product
+	When I press on the wishlist button
+	Then I should see the error message
+
+Scenario: Compare three products
+	Given That I am on the product page
+	And I have selected a product
+	When I press on the Add to compare button
+	And I press on the Compare button
+	Then I should see the selected items side by side
---
+++ Engineering40AutomationPracticeProject/Steps/ProductFunctionSteps.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace Engineering40AutomationPracticeProject.Pages
+{
+    [Binding]
+    public class ProductFunctionSteps
+    {
+        [Given(@"That I am on the product page")]
+        public void GivenThatIAmOnTheProductPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I am logged in")]
+        public void GivenIAmLoggedIn()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have selected a product")]
+        public void GivenIHaveSelectedAProduct()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I press on the wishlist button")]
+        public void WhenIPressOnTheWishlistButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I press on the Add to compare button")]
+        public void WhenIPressOnTheAddToCompareButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I press on the Compare button")]
+        public void WhenIPressOnTheCompareButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should see the product on the wishlist")]
+        public void ThenIShouldSeeTheProductOnTheWishlist()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should see the error message")]
+        public void ThenIShouldSeeTheErrorMessage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should see the selected items side by side")]
+        public void ThenIShouldSeeTheSelectedItemsSideBySide()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
