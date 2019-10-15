a333da242f53056ace2503a679d09b21695226b7
Jez Horton <JHorton@spartaglobal.com>
2019-10-15 15:26:58 +0100

    Added the base login for easy test use

--- Engineering40AutomationPracticeProject/Class1.cs
+++ Engineering40AutomationPracticeProject/Class1.cs
@@ -3,11 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium.Support.PageObjects;
+
 
 namespace Engineering40AutomationPracticeProject
 {
-    public class Class1
-    {
 
+    public class Base
+    {
+        public IWebDriver driver;
+        public Base(IWebDriver Driver)
+        {
+            this.driver = Driver;
+            PageFactory.InitElements(driver, this);
+        }
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+
+        }
+        [FindsBy(How = How.Id, Using = "passwd")]
+        public IWebElement passwordField;
+        [FindsBy(How = How.Id, Using = "email")]
+        public IWebElement emailField;
+        [FindsBy(How = How.Id, Using = "SubmitLogin")]
+        public IWebElement submitButton;
+        public void Navigate(string url)
+        {
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(url);
+        }
+        public void LogIn()
+        {
+            string password = "PAssword";
+            string email = "steve@admin.com";
+            Navigate("http://automationpractice.com/index.php?controller=authentication&back=my-account");
+            emailField.SendKeys(email);
+            passwordField.SendKeys(password);
+            submitButton.Click();
+        }
     }
+
+        [Binding]
+        public class LoginStep
+        {
+
+        private IWebDriver driver;
+        [Given(@"I am on the login page")]
+            public void GivenIAmOnTheLoginPage()
+            {
+                driver = new ChromeDriver();
+                Base ba = new Base(driver);
+                ba.LogIn();
+            }
+            [AfterScenario]
+            public void DisposeWebDriver()
+            {
+                driver.Close();
+            }
+        }
+
 }
---
+++ Engineering40AutomationPracticeProject/SpecFlowFeature1.feature
@@ -0,0 +1,7 @@
+﻿Feature: SpecFlowFeature1
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+Scenario: Add two numbers
+	Given I am on the login page
