5a3a4f6e38af4040de5f1519a9b8acd4ea16a79d
Nazir Ahmed <NAhmed@spartaglobal.com>
2019-10-15 16:25:46 +0100

    Items In Cart Feature and Steps

---
+++ Engineering40AutomationPracticeProject/Features/ItemsInCart.feature
@@ -0,0 +1,17 @@
+﻿Feature: Items In Cart
+		As a User
+		I want to be able to check the items in my cart
+		So that I can proceed to the checkout.
+
+
+Scenario: View items in Shopping cart
+
+    	Given I have added the items to my shopping cart
+    	When I click on Cart
+    	Then I should see the items in the Cart
+
+Scenario: No items in Cart message
+
+		Given I have not added any items to my cart
+		When I click on the Cart button
+		Then there should be a Cart message. 
---
+++ Engineering40AutomationPracticeProject/Steps/ItemsInCartSteps.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Engineering40AutomationPracticeProject.Steps
+{
+    [Binding]
+    public class ItemsInCartSteps
+    {
+        [Given(@"I have added the items to my shopping cart")]
+        public void GivenIHaveAddedTheItemsToMyShoppingCart()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have not added any items to my cart")]
+        public void GivenIHaveNotAddedAnyItemsToMyCart()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I click on Cart")]
+        public void WhenIClickOnCart()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I click on the Cart button")]
+        public void WhenIClickOnTheCartButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should see the items in the Cart")]
+        public void ThenIShouldSeeTheItemsInTheCart()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"there should be a Cart message\.")]
+        public void ThenThereShouldBeACartMessage_()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
