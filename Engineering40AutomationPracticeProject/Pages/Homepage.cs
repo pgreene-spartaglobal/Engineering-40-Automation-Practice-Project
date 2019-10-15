6911106f9f40661bb895eb12df69415f490cf1ff
Dipak Mukesh <DMukesh@spartaglobal.com>
2019-10-15 14:35:39 +0100

    added homepage.cs on page folder

---
+++ Engineering40AutomationPracticeProject/Pages/Homepage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace Engineering40AutomationPracticeProject.Pages
+{
+    class Homepage
+    {
+        private readonly IWebDriver driver;
+
+        public Homepage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public void GoToPage()
+        {
+            driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
+        }
+    }
+}
