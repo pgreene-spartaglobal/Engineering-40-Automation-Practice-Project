016f8dffce089bfc865f24b016e07b8bb35592be
Ciaran Cullen <CCullen@spartaglobal.com>
2019-10-16 15:27:46 +0100

    Added Personal Info Check feature and blank steps page

---
+++ Engineering40AutomationPracticeProject/Features/PersonalInfoCheck.feature
@@ -0,0 +1,24 @@
+﻿Feature: personalInfoCheck
+	in order to login into my account
+	as a user
+	i want to see my personal infomation
+
+Scenario: Check gender
+	Given I have logged in
+	And I pressed my personal information
+	Then I should see appropriate gender
+
+Scenario: Check first name
+	Given I have logged in
+	And I pressed my personal information
+	Then I should see appropriate first name
+
+Scenario: Check last name
+	Given I have logged in
+	And I pressed my personal information
+	Then I should see appropriate last name
+
+Scenario: Check Email address
+	Given I have logged in
+	And I pressed my personal information
+	Then I should see appropriate email address
---
+++ Engineering40AutomationPracticeProject/Steps/PersonalInfoCheckSteps.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium.Support.PageObjects;
+using Engineering40AutomationPracticeProject.Pages;
+
+namespace Engineering40AutomationPracticeProject.Steps
+{
+    [Binding]
+    public class PersonalInfoCheckSteps
+    {
+        [Given(@"I have logged in")]
+        public void GivenIHaveLoggedIn()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I pressed my personal information")]
+        public void GivenIPressedMyPersonalInformation()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should see appropriate gender")]
+        public void ThenIShouldSeeAppropriateGender()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should see appropriate first name")]
+        public void ThenIShouldSeeAppropriateFirstName()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should see appropriate last name")]
+        public void ThenIShouldSeeAppropriateLastName()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should see appropriate email address")]
+        public void ThenIShouldSeeAppropriateEmailAddress()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
