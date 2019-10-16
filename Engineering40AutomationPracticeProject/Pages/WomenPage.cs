5be340608d03c883dab48b8a89985624fade7a6e
Seng Fai Chan <SChan@spartaglobal.com>
2019-10-16 10:04:05 +0100

    product page, women page, editing features

---
+++ Engineering40AutomationPracticeProject/Pages/WomenPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace Engineering40AutomationPracticeProject.Pages
+{
+    class WomenPage: ProductPage
+    {
+        public WomenPage(IWebDriver driver) : base(driver)
+        {
+            GoToPage();
+        }
+
+        public void GoToPage()
+        {
+            driver.Navigate().GoToUrl("http://automationpractice.com/index.php?id_category=3&controller=category");
+        }
+    }
+}
