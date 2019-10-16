ca9ac5be26a658b3df53368bb64cf3a9b01d6847
Dipak Mukesh <DMukesh@spartaglobal.com>
2019-10-16 16:10:06 +0100

    item in cart test passed

--- Engineering40AutomationPracticeProject/Pages/CartPage.cs
+++ Engineering40AutomationPracticeProject/Pages/CartPage.cs
@@ -21,8 +21,11 @@ namespace Engineering40AutomationPracticeProject.Pages
         [FindsBy(How = How.XPath, Using = "//*[@id='center_column']/p")]
         private IWebElement EmptyShoppingCart;
 
-        [FindsBy(How = How.ClassName, Using = "cart_summary")]
+        [FindsBy(How = How.Id, Using = "summary_products_quantity")]
         private IWebElement CartSummary;
+
+        [FindsBy(How = How.ClassName, Using = "cart_quantity_input")]
+        private IWebElement QtySummary;
 
         [FindsBy(How = How.ClassName, Using = "cart_quantity_delete")]
         private IWebElement DeleteCart;
@@ -31,6 +34,16 @@ namespace Engineering40AutomationPracticeProject.Pages
         public string emptyShoppingCart()
         {
             return EmptyShoppingCart.Text;
+        }
+
+        public string ShoppingCartContains()
+        {
+            return CartSummary.Text;
+        }
+
+        public string QtyCartContains()
+        {
+            return QtySummary.GetAttribute("value");
         }
 
         public void deleteCart()
