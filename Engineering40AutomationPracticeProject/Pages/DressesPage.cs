1415d7f939d050c5684759efd50d8c4b6ba990bf
Seng Fai Chan <SChan@spartaglobal.com>
2019-10-16 10:19:13 +0100

    added tshirt and dresses

---
+++ Engineering40AutomationPracticeProject/Pages/DressesPage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium;
+
+namespace Engineering40AutomationPracticeProject.Pages
+{
+    class DressesPage: ProductPage
+    {
+        public DressesPage(IWebDriver driver) : base(driver)
+        {
+
+        }
+    }
+}
---
+++ Engineering40AutomationPracticeProject/Pages/TShirtPage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium;
+
+namespace Engineering40AutomationPracticeProject.Pages
+{
+    class TShirtPage:ProductPage
+    {
+        public TShirtPage(IWebDriver driver) : base(driver)
+        {
+
+        }
+    }
+}
