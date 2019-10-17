a1e479cd9b27bd384ccc2543f4f3f75d549aedb3
Ciaran Cullen <CCullen@spartaglobal.com>
2019-10-17 13:36:03 +0100

    Added tests and code for wishlist testing.

---
+++ Engineering40AutomationPracticeProject/Features/WishlistEdit.feature
@@ -0,0 +1,39 @@
+﻿Feature: WishlistEdit
+	in order to login into my account
+	as a user
+	i want to see my wishlist and edit it
+
+Scenario: Create new wishlist
+	Given I have logged in correctly
+	And i have pressed the wishlist button
+	And i have given my wishlist a name
+	And i pressed the save wishlist button
+	Then i should see the new wishlist
+
+Scenario: check date on new wishlist
+	Given I have logged in correctly
+	And i have pressed the wishlist button
+	And i have given my wishlist a name
+	And i pressed the save wishlist button
+	Then i should see the correct date
+
+Scenario: check add to wishlist
+	Given I have logged in correctly
+	And i have pressed the wishlist button
+	And i have given my wishlist a name
+	And i pressed the save wishlist button
+	And i have pressed on a item
+	And i have added it to a wishlist
+	And i go back to the wishlist page
+	And i click on view
+	Then i should see the item i added
+
+Scenario: check delete works with new wishlist
+	Given I have logged in correctly
+	And i have pressed the wishlist button
+	And i have given my wishlist a name
+	And i pressed the save wishlist button
+	And i delete the new wishlist
+	Then i should not be able to see the wishlist
+
+	
---
+++ Engineering40AutomationPracticeProject/Steps/WishlistEditSteps.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using Engineering40AutomationPracticeProject.Pages;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace Engineering40AutomationPracticeProject.Steps
+{
+    [Binding]
+    public class WishlistEditSteps
+    {
+        IWebDriver driver;
+        LoginPage ba;
+        WishlistPage wishlistPage;
+        [Given(@"I have logged in correctly")]
+        public void GivenIHaveLoggedInCorrectly()
+        {
+            driver = new ChromeDriver();
+            ba = new LoginPage(driver);
+            wishlistPage = new WishlistPage(driver);
+            ba.CiaranLogIn("a@b.a", "ababa");
+        }
+        
+        [Given(@"i have pressed the wishlist button")]
+        public void GivenIHavePressedTheWishlistButton()
+        {
+            wishlistPage.PressWishlistButton();
+        }
+        
+        [Given(@"i have given my wishlist a name")]
+        public void GivenIHaveGivenMyWishlistAName()
+        {
+            wishlistPage.TypeWishlistName("NewwishList");
+        }
+        
+        [Given(@"i pressed the save wishlist button")]
+        public void GivenIPressedTheSaveWishlistButton()
+        {
+            wishlistPage.SaveNewWishlist();
+        }
+        
+        [Given(@"i have pressed on a item")]
+        public void GivenIHavePressedOnAItem()
+        {
+            wishlistPage.PressNewItem();
+        }
+        
+        [Given(@"i have added it to a wishlist")]
+        public void GivenIHaveAddedItToAWishlist()
+        {
+            wishlistPage.AddItemToWishlist();
+        }
+        
+        [Given(@"i go back to the wishlist page")]
+        public void GivenIGoBackToTheWishlistPage()
+        {
+            wishlistPage.GoBackToWishlistPage();
+        }
+        
+        [Given(@"i click on view")]
+        public void GivenIClickOnView()
+        {
+            wishlistPage.ClickOnViewButton();
+        }
+        
+        [Given(@"i delete the new wishlist")]
+        public void GivenIDeleteTheNewWishlist()
+        {
+            wishlistPage.ClickOnDeleteButton();
+        }
+        
+        [Then(@"i should see the new wishlist")]
+        public void ThenIShouldSeeTheNewWishlist()
+        {
+            string result = wishlistPage.GetWishlistName();
+            StringAssert.Contains(result, "NewwishList");
+        }
+        
+        [Then(@"i should see the correct date")]
+        public void ThenIShouldSeeTheCorrectDate()
+        {
+            string result = wishlistPage.GetDate();
+            StringAssert.Contains(result, "2019-10-17");
+        }
+        
+        [Then(@"i should see the item i added")]
+        public void ThenIShouldSeeTheItemIAdded()
+        {
+            string result = wishlistPage.GetWishlistItem();
+            StringAssert.Contains(result, "Printed Chiffon Dress");
+        }
+        
+        [Then(@"i should not be able to see the wishlist")]
+        public void ThenIShouldNotBeAbleToSeeTheWishlist()
+        {
+            string result = wishlistPage.GetWishlistName();
+            StringAssert.DoesNotContain(result, "NewwishList");
+        }
+    }
+}
