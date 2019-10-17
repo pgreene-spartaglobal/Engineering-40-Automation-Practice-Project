8ada9385f950cdd926895301846133fb1ae39cd6
Jez Horton <JHorton@spartaglobal.com>
2019-10-17 11:35:43 +0100

    Started on the order pages

---
+++ Engineering40AutomationPracticeProject/Steps/CheckingOrderPage.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Engineering40AutomationPracticeProject.Steps
+{
+    [Binding]
+    public class CheckingOrdersPageSteps
+    {
+        [Given(@"I have logged into the account")]
+        public void GivenIHaveLoggedIntoTheAccount()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have clicked view orders")]
+        public void GivenIHaveClickedViewOrders()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have clicked sort price")]
+        public void GivenIHaveClickedSortPrice()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have clicked view details")]
+        public void GivenIHaveClickedViewDetails()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have selected the product")]
+        public void GivenIHaveSelectedTheProduct()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have wrote a message")]
+        public void GivenIHaveWroteAMessage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have clicked send")]
+        public void GivenIHaveClickedSend()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have clicked reorder")]
+        public void GivenIHaveClickedReorder()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should see the orders page")]
+        public void ThenIShouldSeeTheOrdersPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"The orders should be ordered by price being minimum first")]
+        public void ThenTheOrdersShouldBeOrderedByPriceBeingMinimumFirst()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should be on the correct page for that order")]
+        public void ThenIShouldBeOnTheCorrectPageForThatOrder()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should see a successful message sent")]
+        public void ThenIShouldSeeASuccessfulMessageSent()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should be on the checkout page")]
+        public void ThenIShouldBeOnTheCheckoutPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
