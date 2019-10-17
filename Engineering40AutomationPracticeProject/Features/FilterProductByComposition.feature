491b88bd71310b493b5455d1829087375e8ebec7
Philip Greene <PGreene@spartaglobal.com>
2019-10-17 13:56:23 +0100

    Wrote composition scenario and generated composition steps file

---
+++ Engineering40AutomationPracticeProject/Features/FilterProductByComposition.feature
@@ -0,0 +1,19 @@
+﻿Feature: FilterProductByComposition
+	In order to select the items I want
+	As a user
+	I want to be able to filter the result by different composition
+
+Scenario: Viewing the Cotton items
+	Given I am on the Women Page
+	When I tick the Cotton check box
+	Then the result should be showing Polyester items
+
+Scenario: Viewing the Polyester items
+	Given I am on the Women Page
+	When I tick the Polyester check box
+	Then the result should be showing Cotton items
+
+Scenario: Viewing the Viscose items
+	Given I am on the Women Page
+	When I tick the Viscose check box
+	Then the result should be showing Cotton items
---
+++ Engineering40AutomationPracticeProject/Steps/FilterProductByCompositionSteps.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Engineering40AutomationPracticeProject.Steps
+{
+    [Binding]
+    public class FilterProductByCompositionSteps
+    {
+        [Given(@"I am on the Women Page")]
+        public void GivenIAmOnTheWomenPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I tick the Cotton check box")]
+        public void WhenITickTheCottonCheckBox()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I tick the Polyester check box")]
+        public void WhenITickThePolyesterCheckBox()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I tick the Viscose check box")]
+        public void WhenITickTheViscoseCheckBox()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should be showing Polyester items")]
+        public void ThenTheResultShouldBeShowingPolyesterItems()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should be showing Cotton items")]
+        public void ThenTheResultShouldBeShowingCottonItems()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
